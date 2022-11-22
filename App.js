e34c64f7e8bd2f6f6397468157d8dd0c8028db82
Matt Simpson <matt.rc.simpson@gmail.com>
2022-11-22 18:35:28 +0200

    add screens

--- App.js
+++ App.js
@@ -1,20 +1,2 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to eHana!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { } from '@react-navigation/native'
+import { } from '@react-navigation/stack'
---
+++ Screens/Home.js
@@ -0,0 +1,14 @@
+import { View, Text, FlatList, StyleSheet, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { firebase } from '../config';
+import { FontAwesome } from '@expo/vector-icons';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
