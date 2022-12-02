cc36120ad9ba403f785100b8898c19c4bd4494cb
Matt Simpson <matt.rc.simpson@gmail.com>
2022-12-02 18:08:01 +0200

    add styling

--- App.js
+++ App.js
@@ -1,24 +1,68 @@
+import { View, Text, FlatList, StyleSheet, Pressable, TextInput, TouchableOpacity, Keyboard } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from './Screens/Home';
+import Tasks from './Screens/Tasks';
 import Detail from './Screens/Detail';
+import Home from './Screens/Home';
+import About from './Screens/About';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const Stack = createStackNavigator()
 
-export default function App() {
+const App = () => {
+  
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle:{
+            backgroundColor: '#F3F3F3',
+          },
+          headerTitleAlign: "center",
+          headerTitleStyle: {
+            color: "#35DCD2",
+          }
+        }}
+      >
         <Stack.Screen
           name='Home'
           component={Home}
+          options={{
+            title: 'eHana'
+          }}
+        />
+        <Stack.Screen
+          name='About'
+          component={About}
+          options={{
+            title: 'eHana'
+          }}
+        />
+        <Stack.Screen
+          name='Tasks'
+          component={Tasks}
+          options={{
+            title: 'eHana'
+          }}
         />
         <Stack.Screen
           name='Detail'
           component={Detail}
+          options={{
+            title: 'eHana'
+          }}
         />
       </Stack.Navigator>
+      <View>
+        <Text>
+          Footer
+        </Text>
+      </View>
     </NavigationContainer>
   );
+
 }
+
+
+export default App
